49e52f9e890f1a4c82c391e1b888f46741ddff36
brittneypostma <brittney.postma@hotmail.com>
2021-07-30 15:56:01 -0400

    icons sizing

--- src/lib/components/layout/Nav.svelte
+++ src/lib/components/layout/Nav.svelte
@@ -1,24 +1,25 @@
 <script>
-	const item = `w-8 h-8 sm:w-12 sm:h-12 rounded-full bg-transparent p-2 hover:bg-primary transition-colors grid grid-cols-1 justify-items-center content-start`
+	const item = `transition-colors grid grid-cols-1 justify-items-center content-start`
 	const link = `grid grid-cols-1 justify-items-center content-start gap-1`
+	const icon = `bg-transparent w-8 h-8 sm:w-12 sm:h-12 p-2 hover:bg-primary transition-colors rounded-full`
 </script>
 
 <nav class="px-2 pt-2 pb-4 w-full">
 	<ul class="flex space-x-2 justify-around items-center max-w-sm mx-auto text-xs font-bold">
-		<li class={item}><a href="" class={link}><img src="/icons/home.svg" alt="Home" />Home</a></li>
+		<li class={item}><a href="" class={link}><img src="/icons/home.svg" alt="Home" class={icon} />Home</a></li>
 		<li class={item}>
-			<a href="" class={link}><img src="/icons/search.svg" alt="Search" />Search</a>
+			<a href="" class={link}><img src="/icons/search.svg" alt="Search" class={icon} />Search</a>
 		</li>
 		<li
-			class="w-8 h-8 sm:w-12 sm:h-12 bg-basics-900 grid grid-cols-1 justify-items-center content-start rounded-full p-2 hover:bg-primary transition-colors"
+			class="grid grid-cols-1 justify-items-center content-start"
 		>
-			<a href="" class={link}><img src="/icons/bag.png" alt="Cart" />Cart</a>
+			<a href="" class={link}><img src="/icons/bag.png" alt="Cart" class={`bg-basics-900 ${icon}`} />Cart</a>
 		</li>
 		<li class={item}>
-			<a href="" class={link}><img src="/icons/heart.svg" alt="Favorites" />Favorites</a>
+			<a href="" class={link}><img src="/icons/heart.svg" alt="Favorites" class={icon} />Favorites</a>
 		</li>
 		<li class={item}>
-			<a href="" class={link}><img src="/icons/profile.svg" alt="Profile" />Profile</a>
+			<a href="" class={link}><img src="/icons/profile.svg" alt="Profile" class={icon} />Profile</a>
 		</li>
 	</ul>
 </nav>
