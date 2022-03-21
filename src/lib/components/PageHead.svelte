8ed6593c4935d8403cee4c09f53e94cf47274444
brittneypostma <brittney.postma@hotmail.com>
2022-03-21 12:14:43 -0400

    og image

--- src/lib/components/PageHead.svelte
+++ src/lib/components/PageHead.svelte
@@ -16,18 +16,18 @@
 	<!-- Google / Search Engine Tags -->
 	<meta itemprop="name" content={title} />
 	<meta itemprop="description" content={description} />
-	<meta itemprop="image" content={ogImage} />
+	<meta itemprop="image" content={ogImage || '/favicon.png'} />
 
 	<!-- Facebook Meta Tags -->
 	<meta property="og:url" content={siteUrl} />
 	<meta property="og:type" content="website" />
 	<meta property="og:title" content={title} />
 	<meta property="og:description" content={description} />
-	<meta property="og:image" content={ogImage} />
+	<meta property="og:image" content={ogImage || '/favicon.png'} />
 
 	<!-- Twitter Meta Tags -->
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:title" content={title} />
 	<meta name="twitter:description" content={description} />
-	<meta name="twitter:image" content={ogImage} />
+	<meta name="twitter:image" content={ogImage || '/favicon.png'} />
 </svelte:head>
---
+++ src/lib/utils/products.js
@@ -0,0 +1,36 @@
+
+
+export const products = [
+  {
+    id: 1,
+    name: 'Easter',
+    price: '$100',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    image: '/images/4th-of-july/4th-gnome-pillow.png'
+  },
+  {
+    id: 2,
+    name: 'Product 2',
+    price: '$200',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    image: '/images/4th-of-july/4th-gnome-pillow.png'
+
+  },
+
+  {
+    id: 3,
+    name: 'Product 3',
+    price: '$300',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    image: '/images/4th-of-july/4th-gnome-pillow.png'
+
+  },
+  {
+    id: 4,
+    name: 'Product 4',
+    price: '$400',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    image: '/images/4th-of-july/4th-gnome-pillow.png'
+
+  }
+]
