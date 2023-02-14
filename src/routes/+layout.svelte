ecb7509e1cee79572b04126108813a134d4f5447
brittneypostma <bdesignedwebdev@gmail.com>
2023-02-14 09:11:06 -0500

    margin

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -34,14 +34,15 @@
 
 <div class="grid grid-cols-1 grid-rows-layout content-start min-h-full relative">
 	<Header />
-	<Categories />
-	<main class="mb-20">
+	<main class="mb-36">
 		{#if !$page.route.id.includes('contact')}
 			<Banner />
 		{/if}
 		<div id="paypal-button-container" class="flex justify-center" />
-		<div class="max p-4 lg:p-8 overflow-y-auto mb-32 lg:mb-0">
+		<div class="max p-4 lg:p-8 overflow-y-auto lg:mb-0">
 			<slot />
 		</div>
 	</main>
+
+	<Categories />
 </div>
